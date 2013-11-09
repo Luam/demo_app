8c89fa94fc40b4945aec4a893ba6bc5c97be1d15
Luam <luam13@gmail.com>
2013-11-09 11:11:21 -0500

    db

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20120521030403) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  
   end
 
 end
